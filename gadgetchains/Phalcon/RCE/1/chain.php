6de2b722b12166832214ea65879c9cc11e0bb961
cf <Charles F>
2017-07-09 17:23:45 +0200

    Small adjustments

--- gadgetchains/Phalcon/RCE/1/chain.php
+++ gadgetchains/Phalcon/RCE/1/chain.php
@@ -2,13 +2,16 @@
 
 namespace GadgetChain\Phalcon;
 
-class RCE extends \PHPGGC\GadgetChain\RCE
+class RCE1 extends \PHPGGC\GadgetChain\RCE
 {
-    public $version = '< 1.2.3';
+    public $version = '<= 1.2.2';
     public $vector = '__wakeup';
     public $author = 'Raz0r';
     public $informations =  'This chain does not expect parameters, will eval()' .
-                            ' any code supplied in php://input (i.e. POST data)';
+                            ' any code supplied in php://input (i.e. POST data).' . "\n" .
+                            'Requires allow_url_include = true.';
+    # No parameters expected
+    public $parameters = [];
 
     public function generate(array $parameters)
     {
