e4d52443c45094c214c92286c02927c46413ace0
Blaklis <blaklis.nirylis@gmail.com>
2019-02-03 02:39:34 +0100

    Adding informations on Drupal7 gadget

--- gadgetchains/Drupal7/RCE/1/chain.php
+++ gadgetchains/Drupal7/RCE/1/chain.php
@@ -7,6 +7,7 @@ class RCE1 extends \PHPGGC\GadgetChain\RCE
     public $version = '>7.0.8';
     public $vector = '__destruct';
     public $author = 'Blaklis';
+    public $informations = 'You will need to post form_build_id=DrupalRCE to /?q=system/ajax once the payload is unserialized';
 
     public function generate(array $parameters)
     {
