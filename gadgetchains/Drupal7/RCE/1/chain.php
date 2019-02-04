f09e762332e87f3b79bee68d824182242e24277f
real <Charles F>
2019-02-04 10:23:04 +0100

    Updated README and versions

--- gadgetchains/Drupal7/RCE/1/chain.php
+++ gadgetchains/Drupal7/RCE/1/chain.php
@@ -4,7 +4,7 @@ namespace GadgetChain\Drupal7;
 
 class RCE1 extends \PHPGGC\GadgetChain\RCE
 {
-    public $version = '>7.0.8';
+    public $version = '7.0.8 < ?';
     public $vector = '__destruct';
     public $author = 'Blaklis';
     public $informations = 'You will need to post form_build_id=DrupalRCE to /?q=system/ajax once the payload is unserialized';
