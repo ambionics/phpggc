a3489387f03cccf7558170e3b295ab45aceaa741
real <Charles F>
2019-02-26 18:16:49 +0100

    #34: Updated Guzzle/RCE1's description to reflect that FnStream cannot be deserialized anymore.

--- gadgetchains/Guzzle/RCE/1/chain.php
+++ gadgetchains/Guzzle/RCE/1/chain.php
@@ -7,7 +7,11 @@ class RCE1 extends \PHPGGC\GadgetChain\RCE
     public $version = '6.0.0 <= 6.3.2';
     public $vector = '__destruct';
     public $author = 'proclnas';
-    public $informations = '';
+    public $informations = '
+        This chain requires GuzzleHttp\Psr7 < 1.5.0, because FnStream cannot be
+        deserialized afterwards.
+        See https://github.com/ambionics/phpggc/issues/34
+    ';
 
 
     public function generate(array $parameters)
