d77688519c5290e0461111fcc75873226be866ef
erwanlr <erwan.lr@gmail.com>
2019-04-28 00:11:18 +0200

    Updates YT link to BSide conf

--- gadgetchains/WordPress/RCE/WooCommerce1/chain.php
+++ gadgetchains/WordPress/RCE/WooCommerce1/chain.php
@@ -7,7 +7,7 @@ class RCE1 extends \PHPGGC\GadgetChain\RCE
     public static $version = '3.4.0 <= ?';
     public static $vector = '__destruct';
     public static $author = 'erwan_lr';
-    public static $informations = 'Demonstrated at BSide Manchester: https://www.youtube.com/watch?v=GePBmsNJw6Y&feature=youtu.be&t=1763';
+    public static $informations = 'Demonstrated at BSide Manchester: https://www.youtube.com/watch?v=GePBmsNJw6Y&t=1763';
 
     public function generate(array $parameters)
     {
