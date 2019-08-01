916245913d50edff7783a69f2397b452d6682fdd
real <Charles F>
2019-08-01 11:05:44 +0200

    Updated infos

--- gadgetchains/Laravel/RCE/5/chain.php
+++ gadgetchains/Laravel/RCE/5/chain.php
@@ -7,7 +7,10 @@ class RCE5 extends \PHPGGC\GadgetChain\RCE
     public static $version = '5.8.30';
     public static $vector = '__destruct';
     public static $author = 'Phith0n';
-    public static $informations = 'Executes given php code througn eval()';
+    public static $informations = '
+        Executes given PHP code through eval().
+        Requires Mockery, which is in the require-dev package. 
+    ';
     public static $parameters = [
     	'code'
     ];
