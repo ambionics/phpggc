8afba8f38543d080c64b623b3930ec49f7ea607a
real <folcharles@gmail.com>
2020-05-15 17:10:18 +0200

    Fixed versions

--- gadgetchains/CodeIgniter4/RCE/2/chain.php
+++ gadgetchains/CodeIgniter4/RCE/2/chain.php
@@ -4,7 +4,7 @@ namespace GadgetChain\CodeIgniter4;
 
 class RCE2 extends \PHPGGC\GadgetChain\RCE
 {
-    public static $version = '4.0.0-rc.4 <= ?'; // tested on 4.0.2 & 4.0.3
+    public static $version = '4.0.0-rc.4 <= 4.0.3+'; // tested on 4.0.2 & 4.0.3
     public static $vector = '__destruct';
     public static $author = 'eboda';
 
