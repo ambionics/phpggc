bc178b584f787a98d4492e421b503d7828437d92
cfreal <folcharles@mail.com>
2020-12-08 15:12:59 +0100

    Added a little more infos on whiras gadgets.

--- gadgetchains/Monolog/CMD/1/chain.php
+++ gadgetchains/Monolog/CMD/1/chain.php
@@ -4,12 +4,14 @@ namespace GadgetChain\Monolog;
 
 class CMD1 extends \PHPGGC\GadgetChain\Command
 {
-    public static $version = '? <= 2.1';
+    public static $version = '? <= 2.1.1+';
     public static $vector = '__destruct';
     public static $author = 'whira';
     public static $information = '
         This chain will target debian based distribution (exim4 MTA) as it will perform a command injection on mail() 
         and use exim4 extended strings, the payload is `/bin/bash -c "$command"`.
+        As this GC requires a setup that is specific, you should have better results by running Monolog/RCE1 or
+        Monolog/RCE2 instead.
     ';
 
     public function generate(array $parameters)
