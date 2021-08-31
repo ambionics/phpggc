9ec666eb2eadf48a414061b90fa112078488a7ed
zhcy2018 <980514750@qq.com>
2021-08-31 15:28:15 +0800

    update information

--- gadgetchains/ThinkPHP/FW/2/chain.php
+++ gadgetchains/ThinkPHP/FW/2/chain.php
@@ -9,9 +9,7 @@ class FW2 extends \PHPGGC\GadgetChain\FileWrite
     public static $author = 'zcy2018';
     public static $information = '
         We do not have full control of the path. Also, the path will turn to
-        a long hex value(md5) depends on the payload, and the payload have 
-        some limitation as well. For stablity, I just fix the path and data. 
-        Your file path will be ROMOTE_PATH/3b58a9545013e88c7186db11bb158c44.php.
+        a long hex value(md5). Your file path will be ROMOTE_PATH/3b58a9545013e88c7186db11bb158c44.php.
         Tested on Windows with php7.3.4 and apache2.4.39.
     ';
     public function generate(array $parameters)
