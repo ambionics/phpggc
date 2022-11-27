6eef11c7af04bdff16e24bc10d8b7e747f43b43e
CyanM0un <75713958+CyanM0un@users.noreply.github.com>
2022-11-27 16:50:16 +0800

    Update chain.php

--- gadgetchains/Monolog/FW/2/chain.php
+++ gadgetchains/Monolog/FW/2/chain.php
@@ -4,7 +4,7 @@ namespace GadgetChain\Monolog;
 
 class FW2 extends \PHPGGC\GadgetChain\FileWrite
 {
-    public static $version = '3.0.0 <= 3.1.0+';
+    public static $version = '-2.0.0+';
     public static $vector = '__destruct';
     public static $author = 'CyanM0un';
 
