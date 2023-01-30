aeacd6700cde0ef559a9c37547143a4f07381d2d
cfreal <folcharles@gmail.com>
2023-01-30 19:51:29 +0100

    Updated versions for Symfony/FD1

--- gadgetchains/Symfony/FD/1/chain.php
+++ gadgetchains/Symfony/FD/1/chain.php
@@ -4,7 +4,7 @@ namespace GadgetChain\Symfony;
 
 class FD1 extends \PHPGGC\GadgetChain\FileDelete
 {
-    public static $version = 'v3.2.7 <= v3.4.25 v4.0.0 <= v4.1.11 v4.2.0+';
+    public static $version = 'v3.2.7 <= v3.4.25 v4.0.0 <= v4.1.11 v4.2.0 <= v4.2.6';
     public static $vector = '__destruct';
     public static $author = 'CyanM0un';
 
