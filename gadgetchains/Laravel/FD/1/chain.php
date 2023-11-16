f0772b752e8362f8340de2fe1dad1f372d1f8fad
MaxRio87 <67597162+MaxRio87@users.noreply.github.com>
2023-11-16 22:47:30 +0100

    Update chain.php

--- gadgetchains/Laravel/FD/1/chain.php
+++ gadgetchains/Laravel/FD/1/chain.php
@@ -7,7 +7,7 @@ class FD1 extends \PHPGGC\GadgetChain\FileDelete
     public static $version = '*';
     public static $vector = '__destruct';
     public static $author = 'Maxime Rinaudo (www.fenrisk.com)';
-    public static $information = 'Deletes an arbitrary file in Pail plugin';
+    public static $information = 'Deletes an arbitrary file in Laravel\'s Pail plugin';
 
     public function generate(array $parameters)
     {
