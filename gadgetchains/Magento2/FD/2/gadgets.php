c8c08eeaacbf4710af8eba7f5bc2d301d09b91ca
cfreal <folcharles@gmail.com>
2023-11-17 09:52:52 +0100

    Fixed information and codestyle.

--- gadgetchains/Magento2/FD/2/gadgets.php
+++ gadgetchains/Magento2/FD/2/gadgets.php
@@ -1,24 +1,35 @@
 <?php
 
-namespace Magento\Framework\Filesystem\Driver {
-    class File {}
+namespace Magento\Framework\Filesystem\Driver
+{
+    class File
+    {
+    }
 }
 
-namespace Magento\RemoteStorage\Model\Filesystem\Directory {
-    class Write {
-	public $driver;
-	public function __construct() {
+namespace Magento\RemoteStorage\Model\Filesystem\Directory
+{
+    class Write
+    {
+        public $driver;
+
+        public function __construct()
+        {
             $this->driver = new \Magento\Framework\Filesystem\Driver\File();
         }
     }
 }
 
-namespace Magento\RemoteStorage\Model { 
-    class TmpFileCopier {
+namespace Magento\RemoteStorage\Model
+{ 
+    class TmpFileCopier
+    {
 	    public $tmpFiles;
 	    public $tmpDirectoryWrite;
-	    public function __construct($file) {
-            $this->tmpFiles = array('1' => $file);
+
+	    public function __construct($file)
+        {
+            $this->tmpFiles = ['1' => $file];
             $this->tmpDirectoryWrite = new \Magento\RemoteStorage\Model\Filesystem\Directory\Write();
         }		
     }   
