f46655c589e7ade5ac86e8562503429e3d271701
cfreal <folcharles@gmail.com>
2023-11-17 09:49:50 +0100

    Fixed information and codestyle.

--- gadgetchains/Laravel/FD/1/chain.php
+++ gadgetchains/Laravel/FD/1/chain.php
@@ -7,7 +7,7 @@ class FD1 extends \PHPGGC\GadgetChain\FileDelete
     public static $version = '*';
     public static $vector = '__destruct';
     public static $author = 'Maxime Rinaudo';
-    public static $information = 'Deletes an arbitrary file in Laravel\'s Pail plugin';
+    public static $information = 'Requires the Laravel Pail plugin.';
 
     public function generate(array $parameters)
     {
--- gadgetchains/Laravel/FD/1/gadgets.php
+++ gadgetchains/Laravel/FD/1/gadgets.php
@@ -1,18 +1,26 @@
 <?php
 
-namespace Laravel\Pail {
-    class File {
+namespace Laravel\Pail
+{
+    class File
+    {
         public $file;
-        public function __construct($file) {
+
+        public function __construct($file)
+        {
             $this->file = $file;
         }
     }
 }
 
-namespace Laravel\Pail\Console\Commands {
-    class PailCommand {
+namespace Laravel\Pail\Console\Commands
+{
+    class PailCommand
+    {
         public $file;
-        public function __construct($file) {
+
+        public function __construct($file)
+        {
             $this->file = new \Laravel\Pail\File($file);
         }
     }
