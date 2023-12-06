aab3b9afb43c19bf6d941490aec4cfbacc19d0f2
mcdruid <drew@mcdruid.co.uk>
2023-12-06 15:00:12 +0000

    Fixed version for Drupal7/RCE1

--- gadgetchains/Drupal7/RCE/1/chain.php
+++ gadgetchains/Drupal7/RCE/1/chain.php
@@ -4,7 +4,7 @@ namespace GadgetChain\Drupal7;
 
 class RCE1 extends \PHPGGC\GadgetChain\RCE\FunctionCall
 {
-    public static $version = '7.0.8 < ?';
+    public static $version = '7.0.8 <= 7.98';
     public static $vector = '__destruct';
     public static $author = 'Blaklis';
     public static $information = 'You will need to post form_build_id=DrupalRCE to /?q=system/ajax once the payload is unserialized';
