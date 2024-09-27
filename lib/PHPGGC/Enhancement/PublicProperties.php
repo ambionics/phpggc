910b295fc57e0894a4e775df669e4f4bc13dc668
mcdruid <drew@mcdruid.co.uk>
2024-09-27 23:45:22 +0100

    support private properties too

--- lib/PHPGGC/Enhancement/PublicProperties.php
+++ lib/PHPGGC/Enhancement/PublicProperties.php
@@ -4,20 +4,23 @@ namespace PHPGGC\Enhancement;
 
 /**
 * Public Properties
-* Attempts to convert references to protected properties within the serialized
-* payload to public.
+* Attempts to convert references to protected or private properties within the
+* serialized payload to public.
 *
-* This can be useful because when PHP serializes a protected property of an
-* object it prepends the property name with an asterisk surrounded by null
-* bytes, which are easy to lose if the payload is transmitted or stored as plain
-* text without encoding. If that happens, the payload will fail to unserialize
-* because the string length of the property name (and the name itself) will be
-* incorrect.
+* This can be useful because when PHP serializes a non-public property of an
+* object it prepends the property name with an asterisk (for protected) or the
+* class name (for private) surrounded by null bytes, which are easy to lose if
+* the payload is transmitted or stored as plain text without encoding. If that
+* happens, the payload will fail to unserialize because the string length of the
+* property name (and the name itself) will be incorrect.
 *
 * As an added bonus, payloads are slightly smaller without the prefixes.
 *
-* Converting protected properties to public tends to work in more recent PHP
-* versions but can cause problems in older versions (before PHP 7.2).
+* Converting properties to public tends to work in more recent PHP versions but
+* can cause problems in older versions (before PHP 7.2).
+*
+* This functionality may not work properly if a chain includes one or more
+* objects that have a custom serialize / unserialize implementation.
 */
 class PublicProperties extends Enhancement
 {
@@ -30,15 +33,17 @@ class PublicProperties extends Enhancement
     {
        // Encode the payload to make null bytes easier to work with.
        $encoded_payload = urlencode($serialized);
-       preg_match_all('/s%3A([0-9]*)%3A%22%00%2A%00/', $encoded_payload, $matches);
+       preg_match_all('/s%3A([0-9]*)%3A%22%00(([[:alnum:]_]|%2A)*)%00/', $encoded_payload, $matches);
        $replace_pairs = [];
        if (is_array($matches[1])) {
-           $lengths = array_unique($matches[1]);
-           foreach ($lengths as $length) {
-               $replace_pairs['s%3A' . $length . '%3A%22%00%2A%00'] = 's%3A' . $length - 3 . '%3A%22';
+           foreach ($matches[1] as $i => $length) {
+               $search = 's%3A' . $length . '%3A%22%00' . $matches[2][$i] . '%00';
+               $reduction = strlen(urldecode($matches[2][$i])) + 2;
+               $replace = 's%3A' . $length - $reduction . '%3A%22';
+               $replace_pairs[$search] = $replace;
            }
+           $encoded_payload = strtr($encoded_payload, $replace_pairs);
        }
-       $encoded_payload = strtr($encoded_payload, $replace_pairs);
        return urldecode($encoded_payload);
     }
 }
