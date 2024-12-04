ae8ab69e928a4d91c63a6b6665735f75e1525432
mcdruid <drew@mcdruid.co.uk>
2024-12-04 17:30:54 +0000

    rename D9 to just Drupal, plus add details of the SA

--- gadgetchains/Drupal/AT/1/chain.php
+++ gadgetchains/Drupal/AT/1/chain.php
@@ -4,11 +4,11 @@ namespace GadgetChain\Drupal9;
 
 class AT1 extends \PHPGGC\GadgetChain\AccountTakeover
 {
-    public static $version = '9.4.2+';
+    public static $version = '>= 8.0.0 < 10.2.11 || >= 10.3.0 < 10.3.9 || >= 11.0.0 < 11.0.8';
     public static $vector = '__wakeup';
     public static $author = 'mcdruid';
-    public static $information =
-        'Updates the email address for user #1. The user name is also set to
+    public static $information = 'See: https://www.drupal.org/sa-core-2024-007
+        Updates the email address for user #1. The user name is also set to
         "admin", after which it should be possible to request a password reset.';
 
     public function generate(array $parameters)
