af662bb391c2ea78b91bdc17a2f444f44737c741
mcdruid <drew@mcdruid.co.uk>
2024-12-04 17:17:28 +0000

    Drupal/INFO1

--- gadgetchains/Drupal/INFO/1/chain.php
+++ gadgetchains/Drupal/INFO/1/chain.php
@@ -1,14 +1,14 @@
 <?php
 
-namespace GadgetChain\Drupal9;
+namespace GadgetChain\Drupal;
 
 class INFO1 extends \PHPGGC\GadgetChain\PHPInfo
 {
-    public static $version = '9.4.2+';
+    public static $version = '>= 8.0.0 < 10.2.11 || >= 10.3.0 < 10.3.9 || >= 11.0.0 < 11.0.8';
     public static $vector = '__wakeup';
     public static $author = 'mcdruid';
-    public static $information =
-        'This requires PsySH which is bundled with drush. It is common but not
+    public static $information = 'See: https://www.drupal.org/sa-core-2024-007
+        This requires PsySH which is bundled with drush. It is common but not
         mandatory for drush to be installed along with Drupal core. Other PHP
         functions could be executed, but no parameters can be passed.';
 
