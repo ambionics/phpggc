7bb53b5b44a9093bc90c0d91bff3baeaa67f930b
mcdruid <drew@mcdruid.co.uk>
2024-12-08 10:33:20 +0000

    note about commit that will protect __destruct

--- gadgetchains/OpenCart/FW/1/chain.php
+++ gadgetchains/OpenCart/FW/1/chain.php
@@ -7,6 +7,9 @@ class FW1 extends \PHPGGC\GadgetChain\FileWrite
     public static $version = '4.0.0.0 <= 4.0.2.3+';
     public static $vector = '__destruct';
     public static $author = 'mcdruid';
+    public static $information = 'This will stop working when the following:
+    https://github.com/opencart/opencart/commit/087e20dd1cd9b441be5a327fd4b6698744bffb38
+    ..is included in a release.';
 
     public function generate(array $parameters)
     {
