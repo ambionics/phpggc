ba36f3200e8bcb62c08f2373e5f398986411784c
mcdruid <drew@mcdruid.co.uk>
2025-04-04 15:25:35 +0100

    fix version details for RCE/1

--- gadgetchains/OpenCart/RCE/1/chain.php
+++ gadgetchains/OpenCart/RCE/1/chain.php
@@ -4,12 +4,11 @@ namespace GadgetChain\OpenCart;
 
 class RCE1 extends \PHPGGC\GadgetChain\RCE\FunctionCall
 {
-    public static $version = '4.0.0.0 <= 4.0.2.3+';
+    public static $version = '4.0.0.0 < 4.1.0.0';
     public static $vector = '__destruct';
     public static $author = 'mcdruid';
-    public static $information = 'This will stop working when the following:
-    https://github.com/opencart/opencart/commit/087e20dd1cd9b441be5a327fd4b6698744bffb38
-    ..is included in a release.';
+    public static $information = 'This stopped working when this commit landed:
+    https://github.com/opencart/opencart/commit/087e20dd1cd9b441be5a327fd4b6698744bffb38';
 
     public function generate(array $parameters)
     {
