06d28f5af1ae3839698a38572e0cf2b955acbef8
Mirhossein Rahmani <71374198+mir-hossein@users.noreply.github.com>
2025-06-09 13:37:35 +0330

    $author was edited for Monolog/RCE7

--- gadgetchains/Monolog/RCE/7/chain.php
+++ gadgetchains/Monolog/RCE/7/chain.php
@@ -6,7 +6,7 @@ class RCE7 extends \PHPGGC\GadgetChain\RCE\FunctionCall
 {
     public static $version = '1.10.0 <= 2.7.0+';
     public static $vector = '__destruct';
-    public static $author = 'mir-hossein';
+    public static $author = 'mir-hossein (Mirhossein Rahmani)7';
     public static $information = 'Please use this GC only for educational purposes or legal penetration testing. Thank you!';
 
     public function generate(array $parameters)
